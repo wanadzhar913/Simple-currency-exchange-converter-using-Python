fea3ff6a6c0b7c9e9c0783e40f1eead0875bcc94
Kuchai913 <adzhar.faiq@gmail.com>
2022-09-14 22:30:23 +0800

    More exceptions and restrictions on input

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ### Simple-currency-exchange-converter-using-Python
 
 I came across a simple online tutorial for how to create a currency exchange converter using Python. The tutorial can be found [here](https://www.youtube.com/watch?v=snPGUT-Fxa4).
-It makes use of the [Frankfurter API](https://github.com/hakanensari/frankfurter). In their own words they are an *open-source API for current and historical foreign exchange rates published by the European Central Bank*.
-This project also briefly introduced me to Python's Requests library. 
+
+It makes use of the [Frankfurter API](https://github.com/hakanensari/frankfurter). In their own words, Frankfurter API are an *open-source API for current and historical foreign exchange rates published by the European Central Bank*.
+
+I have since modified the code with more robust exception handling, extracted an additional variable (date), as well as limit the currencies that can be converted to and from. Overall, this project briefly introduced me to Python's Requests module, as well as help practice my Exception handling.
--- solution_2.py
+++ solution_2.py
@@ -1,24 +1,62 @@
-# We'll be using the 'requests' library from python.
+# We'll be using the 'requests' library and 'datetime' module from python.
 import requests
 from datetime import datetime
 
-# We now define variables of interest. 
+# We restrict the type of currencies we can convert from and to. 
+iso_codes = requests.get('https://api.frankfurter.app/currencies')
+iso_codes_only = list(iso_codes.json().keys())
 
-rate_date = str(input("Enter the date you'd like the exchange rate to be on (yyyy-mm-dd): "))
-str_to_date = datetime.strptime(rate_date, '%Y-%m-%d').date()
-
+# We now define the variables of interest.
+# Currency to convert from. 
 from_currency = str(input("Enter in the currency you'd like to convert from: ")).upper()
+while from_currency not in iso_codes_only:
+    print('Plese enter a valid country code.')
+    from_currency = str(input("Enter in the currency you'd like to convert from: ")).upper()
 
+# Currency to convert to.
 to_currency = str(input("Enter in the currency you'd like to convert to: ")).upper()
+while to_currency not in iso_codes_only:
+    print('Plese enter a valid country code.')
+    to_currency = str(input("Enter in the currency you'd like to convert from: ")).upper()
 
-amount = float(input("Enter in the amount of money: "))
+# Amount of currency to convert.
+while True:
+    try:
+        amount = float(input("Enter in the amount of money you'd like to convert: "))
+        while amount < 0:
+            print('Please enter a positive amount.')
+            amount = float(input("Enter in the amount of money you'd like to convert: "))
+    except ValueError as e:
+        print("Error: {}".format(e), end = '\n')
+        print("Please enter a valid amount.")
+    except:
+        print("Please enter a suitable date in the required format.")
+    else:
+        break
 
-response = requests.get(f'''https://api.frankfurter.app/latest?amount={amount}&date={str_to_date}&from={from_currency}&to={to_currency}''')
+# Input the date of the currency rate.
+while True:
+    rate_date = str(input("Enter the date you'd like the exchange rate to be on (yyyy-mm-dd): "))
+    try:
+        str_to_date = datetime.strptime(rate_date, '%Y-%m-%d').date()
+    except ValueError as e:
+        print("Error: {}".format(e), end = '\n')
+        print("Please enter a suitable date in the required format.")
+    except:
+        print("Please enter a suitable date in the required format.")
+    else:
+        break
 
-# print(response.status_code) 
-# The above line of code returns a number that indicates the status (200 is OK, 404 is Not Found). 
+response = requests.get(f'''
+https://api.frankfurter.app/latest?amount={amount}&date={str_to_date}&from={from_currency}&to={to_currency}
+''')
+
+# The line of code below returns a number that indicates the status (200 is OK, 404 is Not Found). 
 # You can read more at this link: https://www.geeksforgeeks.org/response-status_code-python-requests/
+print(response.status_code) 
+
 
 # output (amount in converted currency)
-print(f'''{amount} {from_currency} is {response.json()['rates'][to_currency]}{to_currency} on {rate_date}''')
-
+print(f'''
+{amount} {from_currency} is {response.json()['rates'][to_currency]} {to_currency} on {rate_date}
+''')
